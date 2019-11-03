10cd49c5cb620ffdd07b08c10181771b2895da20
augustin <augustin.barillec@gmail.com>
2019-11-03 23:37:54 +0100

    timer is working

--- bluffer/game.py
+++ bluffer/game.py
@@ -1,7 +1,11 @@
 from copy import deepcopy
+from datetime import datetime, timedelta
+import time
+import threading
 from bluffer.utils import get_modal, get_block
 
 game_setup = get_modal(__file__, 'game_setup.json')
+your_guess = get_modal(__file__, 'your_guess.json')
 button_block = get_block(__file__, 'button.json')
 divider_block = get_block(__file__, 'divider.json')
 image_block = get_block(__file__, 'image.json')
@@ -9,8 +13,7 @@ text_block = get_block(__file__, 'text.json')
 
 
 class Game:
-    def __init__(self, trigger_id, channel_id, organizer_id, slack_client):
-        self.trigger_id = trigger_id
+    def __init__(self, channel_id, organizer_id, slack_client):
         self.channel_id = channel_id
         self.organizer_id = organizer_id
         self.slack_client = slack_client
@@ -19,6 +22,16 @@ class Game:
         self.answer = None
         self.time_to_guess = None
         self.time_to_vote = None
+
+        self.start_call = None
+
+        self.players = ''
+
+        self.guesses = dict()
+
+        self.start_datetime = None
+        self.deadline_1 = None
+        self.t1 = None
 
     @staticmethod
     def text_block(message):
@@ -32,10 +45,10 @@ class Game:
         res['elements'][0]['text']['text'] = message
         return res
 
-    def ask_setup(self):
+    def ask_setup(self, trigger_id):
         self.slack_client.api_call(
             "views.open",
-            trigger_id=self.trigger_id,
+            trigger_id=trigger_id,
             view=game_setup)
 
     def collect_setup(self, view):
@@ -49,11 +62,7 @@ class Game:
 
     @property
     def time_remaining_to_guess(self):
-        return 3
-
-    @property
-    def players(self):
-        return ''
+        return (self.deadline_1 - datetime.now()).seconds
 
     @property
     def title_block(self):
@@ -62,7 +71,9 @@ class Game:
     @property
     def organizer_block(self):
         msg = "Set up by <@{}> !".format(self.organizer_id)
-        return self.text_block(msg)
+        res = self.text_block(msg)
+        res['block_id'] = self.organizer_id
+        return res
 
     @property
     def question_block(self):
@@ -97,8 +108,46 @@ class Game:
         ]
 
     def start(self):
-        self.slack_client.api_call(
+        self.start_datetime = datetime.now()
+        self.deadline_1 = self.start_datetime + timedelta(
+            seconds=int(self.time_to_guess)*60)
+
+        self.start_call = self.slack_client.api_call(
             "chat.postMessage",
             channel=self.channel_id,
             text="",
             blocks=self.starting_board)
+
+        self.t1 = threading.Thread(target=self.update_regularly_starting_board)
+        self.t1.start()
+
+    @property
+    def your_guess_modal(self):
+        res = your_guess
+        res['blocks'][0] = self.question_block
+        res['callback_id'] = 'your_guess#{}'.format(self.organizer_id)
+        return res
+
+    def send_your_guess_modal(self, trigger_id):
+        self.slack_client.api_call(
+            "views.open",
+            trigger_id=trigger_id,
+            view=self.your_guess_modal)
+
+    def update_starting_board(self):
+        self.slack_client.api_call(
+            "chat.update",
+            channel=self.channel_id,
+            ts=self.start_call["ts"],
+            text="",
+            blocks=self.starting_board)
+
+    def update_regularly_starting_board(self):
+        while True:
+            self.update_starting_board()
+            time.sleep(0.01)
+
+    def add_guess(self, user_id, view):
+        values = view['state']['values']
+        guess = values['your_guess']['your_guess']['value']
+        self.guesses[user_id] = guess
--- bluffer/main.py
+++ bluffer/main.py
@@ -16,9 +16,9 @@ def command():
     trigger_id = request.form['trigger_id']
     channel_id = request.form['channel_id']
     organizer_id = request.form['user_id']
-    game = Game(trigger_id, channel_id, organizer_id, slack_client)
+    game = Game(channel_id, organizer_id, slack_client)
     games[organizer_id] = game
-    game.ask_setup()
+    game.ask_setup(trigger_id)
     return make_response("", 200)
 
 
@@ -26,6 +26,7 @@ def command():
 def message_actions():
     message_action = json.loads(request.form["payload"])
     user_id = message_action['user']['id']
+
     if message_action["type"] == "view_submission":
         view = message_action["view"]
         if view["callback_id"] == "game_setup":
@@ -33,6 +34,20 @@ def message_actions():
             game = games[organizer_id]
             game.collect_setup(view)
             game.start()
+
+        if view["callback_id"].startswith("your_guess"):
+            organizer_id = view['callback_id'].split('#')[-1]
+            game = games[organizer_id]
+            game.add_guess(user_id, view)
+            game.players += ' <@{}>'.format(user_id)
+            game.update_starting_board()
+
+    if message_action["type"] == "block_actions":
+        trigger_id = message_action['trigger_id']
+        organizer_id = message_action['message']['blocks'][3]['block_id']
+        game = games[organizer_id]
+        game.send_your_guess_modal(trigger_id)
+
     return make_response("", 200)
 
 
